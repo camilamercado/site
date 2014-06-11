a5e81ddbc5e775124b6ced9883147b473ed74273
Camila Mercado <camilamercado11@gmail.com>
2014-06-11 13:59:19 -0400

    work on array

--- template.js
+++ template.js
@@ -59,24 +59,30 @@ function template(){
 
           //slideMod 
 
-        //   frame = 0;
+          count = 0;
 
-        //   $(".slideMod").click(function(){
-        //     frame = frame + 1;
-        //     console.log(frame);
+          $(".slideMod").click(function(){
+            
+            count = count + 1;
+            console.log(count);
 
-        //      for(var j = 0; j < frames.length; j++) {
-        
-        // if(frames[j].framey === frame ) {
+              for(var i = 0; i < pages.length; i++) {
 
-        //      document.querySelector(".window1").style.backgroundImage=frames[i].window;
+                if(pages[i].state === state ) {
 
-        //    }
+                console.log(pages[i].frames[count].image);
+                document.querySelector(".window1").style.backgroundImage=pages[i].frames[count].image;
 
-        //  }
+                // if(pages[i].frames[count] > pages[i].frames[i].index ){
+                //   count === 0;
+                //   return
+                // }
+
+              }
+            }
 
 
-        //   });
+          });
 
           
 
@@ -88,6 +94,13 @@ function template(){
 
 }
 
+
+ // _.each(pages[i].frames, function(slide, i){
+ //            console.log(slide);
+ //            if(word === decision.name.toLowerCase()){
+ //              isMatch=true;
+ //              currentPage = 'page_' + decision.page;
+ //            }
 
 
 
@@ -101,19 +114,26 @@ var pages = [
         'icon' : "url(css/istamp.png)",
         'link' : "http://camilamercado.github.io/dds/orbit.html",
         
-        // frames : [
+        frames : [
          
-        //   { 
-        //     framey : 0,
-        //     'window' : "url(css/dtw.png)"
-        //   },
+          { 
+            index : 0,
+            image : "url(css/dtw.png)"
+          },
           
-        //   { 
-        //     framey : 1,
-        //     'window' : "url(css/img2.png)"
-        //   },
+          { 
+            index : 1,
+            image : "url(css/img2.png)"
+          },
+
+          { 
+            index : 2,
+            image : "url(css/fiction.jpg)"
+          },
+
         
-        // ]
+        ],
+
         'window1' : "url(css/dtw.png)",
         'window2' : "url(css/img2.png)",
         
@@ -126,7 +146,27 @@ var pages = [
         'subTitle' : "2013    ",
         'icon' : "url(css/stamp2.png)", 
         'window1' : "url(css/bi1.png)",
-        'link' : "http://camilamercado.github.io/biching/"
+        'link' : "http://camilamercado.github.io/biching/",
+
+        frames : [
+         
+          { 
+            index : 0,
+            image : "url(css/bi1.png)"
+          },
+          
+          { 
+            index : 1,
+            image : "url(css/bi2.png)"
+          },
+
+          { 
+            index : 2,
+            image : "url(css/bi3.png)"
+          },
+
+        
+        ],
          
       }, 
 
@@ -136,7 +176,27 @@ var pages = [
         'subTitle' : "2012    ",
         'icon' : "url(css/fafi.png)", 
         'window1' : "url(css/still.png)",
-        'link' : ""
+        'link' : "",
+
+        frames : [
+         
+          { 
+            index : 0,
+            image : "url(css/still.png)"
+          },
+          
+          { 
+            index : 1,
+            image : "url(css/fa45.jpg)"
+          },
+
+          { 
+            index : 2,
+            image : "url(css/fafw.png)"
+          },
+
+        
+        ],
          
       },  
 
