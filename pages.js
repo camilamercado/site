fc4dcb769001d1f13cc410c8be845ac5ae5da362
Camila Mercado <theronmercado@theron-mercados-macbook-3.local>
2014-06-18 09:46:35 -0400

    add untracked

---
+++ pages.js
@@ -0,0 +1,103 @@
+
+
+var pages = [
+
+
+      { 
+        'state' : "X",
+        'title' : "Daphne & Telstar",
+        'subTitle' : "2014   ",
+        'icon' : "url(css/istamp.png)",
+        'link' : "http://camilamercado.github.io/dds/orbit.html",
+        
+        frames : [
+         
+          { 
+            image : "url(css/dtw.png)"
+          },
+          
+          { 
+            image : "url(css/shelf.png)"
+          },
+
+          { 
+            image : "url(css/fiction.jpg)"
+          },
+
+          { 
+            image : "url(css/img2.png)"
+          },
+
+        
+        ],
+
+        'window1' : "url(css/dtw.png)",
+        'window2' : "url(css/img2.png)",
+        
+         
+      }, 
+
+      { 
+        'state' : "Y",
+        'title' : "b1/Ching",
+        'subTitle' : "2013    ",
+        'icon' : "url(css/stamp2.png)", 
+        'window1' : "url(css/bi1.png)",
+        'link' : "http://camilamercado.github.io/biching/",
+
+        frames : [
+         
+          { 
+            image : "url(css/bi1.png)"
+          },
+          
+          { 
+            image : "url(css/bi2.png)"
+          },
+
+          { 
+            image : "url(css/bi3.png)"
+          },
+
+          { 
+            image : "url(css/binary.png)"
+          },
+
+        
+        ],
+         
+      }, 
+
+      { 
+        'state' : "Z",
+        'title' : "Actual Size",
+        'subTitle' : "2012    ",
+        'icon' : "url(css/fafi.png)", 
+        'window1' : "url(css/still.png)",
+        'link' : "",
+
+        frames : [
+         
+          { 
+            image : "url(css/still.png)"
+          },
+          
+          { 
+            image : "url(css/ASC.jpg)"
+          },
+
+          { 
+            image : "url(css/fafw.png)"
+          },
+
+          { 
+            image : "url(css/col.png)"
+          },
+
+        
+        ],
+         
+      },  
+
+];
+
