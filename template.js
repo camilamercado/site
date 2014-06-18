108bc3cbf9f433e31d511ec80b70c48ba2463859
Camila Mercado <theronmercado@theron-mercados-macbook-3.local>
2014-06-18 09:41:22 -0400

    new templating

--- template.js
+++ template.js
@@ -1,5 +1,12 @@
 //Template State Call
 
+// if (intent.responsive.name === "tablet"){
+//   console.log(intent.responsive.name);
+// }
+
+
+console.log(intent.responsive.name);
+
 var state = "";
 
 $(".topNav").click(function(){
@@ -89,104 +96,25 @@ function template(){
 
 
 
-var pages = [
 
 
-      { 
-        'state' : "X",
-        'title' : "Daphne & Telstar",
-        'subTitle' : "2014   ",
-        'icon' : "url(css/istamp.png)",
-        'link' : "http://camilamercado.github.io/dds/orbit.html",
-        
-        frames : [
-         
-          { 
-            image : "url(css/dtw.png)"
-          },
-          
-          { 
-            image : "url(css/shelf.png)"
-          },
 
-          { 
-            image : "url(css/fiction.jpg)"
-          },
 
-          { 
-            image : "url(css/img2.png)"
-          },
 
-        
-        ],
 
-        'window1' : "url(css/dtw.png)",
-        'window2' : "url(css/img2.png)",
-        
-         
-      }, 
 
-      { 
-        'state' : "Y",
-        'title' : "b1/Ching",
-        'subTitle' : "2013    ",
-        'icon' : "url(css/stamp2.png)", 
-        'window1' : "url(css/bi1.png)",
-        'link' : "http://camilamercado.github.io/biching/",
 
-        frames : [
-         
-          { 
-            image : "url(css/bi1.png)"
-          },
-          
-          { 
-            image : "url(css/bi2.png)"
-          },
 
-          { 
-            image : "url(css/bi3.png)"
-          },
 
-          { 
-            image : "url(css/binary.png)"
-          },
 
-        
-        ],
-         
-      }, 
 
-      { 
-        'state' : "Z",
-        'title' : "Actual Size",
-        'subTitle' : "2012    ",
-        'icon' : "url(css/fafi.png)", 
-        'window1' : "url(css/still.png)",
-        'link' : "",
 
-        frames : [
-         
-          { 
-            image : "url(css/still.png)"
-          },
-          
-          { 
-            image : "url(css/ASC.jpg)"
-          },
 
-          { 
-            image : "url(css/fafw.png)"
-          },
 
-          { 
-            image : "url(css/col.png)"
-          },
 
-        
-        ],
-         
-      },  
 
-];
+
+
+
+
 
