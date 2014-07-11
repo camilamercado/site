6b03e8205021e3d62da409671c0b8ba12c6e3432
Camila Mercado <theronmercado@new-host.home>
2014-07-11 01:15:58 -0400

    add new as imgs

--- pages.js
+++ pages.js
@@ -80,7 +80,7 @@ var pages = [
         'title' : "Actual Size",
         'subTitle' : "2012    ",
         'icon' : "url(css/actualsize/fafi.png)", 
-        'window1' : "url(css/actualsize/still.png)",
+        'window1' : "url(css/actualsize/sizes.png)",
         'text1' : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquam ligula quis orci eleifend, ut facilisis tellus tempor. Proin suscipit urna a velit varius consequat. Nunc facilisis sem libero, eu sagittis felis porttitor ac. Phasellus semper lacus risus, ac imperdiet lacus volutpat eget. Pellentesque dignissim sed ligula at viverra. Vivamus pulvinar viverra libero, quis luctus mauris. Suspendisse dapibus mauris molestie felis ultricies porta. Proin vel magna dapibus, lobortis ipsum vel, suscipit massa. Suspendisse egestas augue quis orci hendrerit molestie. Donec blandit, nunc vitae cursus suscipit, mauris nisi auctor enim, porta commodo metus orci nec mi. Etiam viverra feugiat nisi, placerat accumsan risus ornare sed. Mauris ante orci, malesuada dignissim sem in, ultricies imperdiet nisl. In hac habitasse platea dictumst. Nam sollicitudin, sapien ac hendrerit convallis, orci diam rhoncus arcu, nec condimentum purus turpis eu est. Cras velit eros, eleifend a commodo nec, egestas ac sapien. Nulla imperdiet vehicula odio, at venenatis magna congue eget. Vestibulum mollis rutrum felis. Maecenas porttitor mattis ante, nec scelerisque quam congue nec. Aenean tristique dapibus leo, fermentum suscipit enim pharetra eu. Mauris luctus, dui at varius faucibus, dolor elit adipiscing orci, a venenatis tellus dui vel eros. Duis bibendum eleifend justo vitae vehicula. Curabitur hendrerit, sapien id mollis venenatis, ante nunc cursus est, sed venenatis augue tellus eget dolor. Nulla ac aliquet nibh, ut scelerisque metus. Nulla non urna sed augue aliquam pellentesque. Phasellus rhoncus metus at mauris mollis dapibus. Aenean laoreet dolor at orci adipiscing mollis. Etiam nec nisi lorem. Vestibulum venenatis nibh et tempor tincidunt. Fusce dapibus felis a tortor interdum, at interdum ligula consequat. Vestibulum a sollicitudin est. Sed enim sem, luctus egestas justo a, molestie accumsan lectus. Integer tristique felis in adipiscing sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae liquam hendrerit aliquam sapien sed mattis. Phasellus pellentesque enim arcu, at euismod odio faucibus eget. Cras lorem felis, lobortis fermentum pretium quis, aliquet et massa. Nam quis ipsum diam. Morbi eu ornare tellus, sed convallis justo. Nulla aliquet purus lobortis lobortis fringilla. Cras sit amet arcu quis turpis varius commodo. Pellentesque id purus eu risus cursus auctor sit amet eget enim. Nulla ut tempus lectus. Cras sollicitudin sapien neque, vel faucibus tellus eleifend et. Aliquam pharetra eget magna in hendrerit. Mauris auctor arcu orci, id euismod metus fringilla nec. Duis convallis odio et porta molestie. Maecenas a magna quis diam euismod mattis. Mauris placerat iaculis gravida.",
         'link' : " ",
         'linky' : "",
@@ -88,15 +88,19 @@ var pages = [
         frames : [
          
           { 
-            image : "url(css/actualsize/still.png)"
+            image : "url(css/actualsize/sizes.png)"
           },
           
           { 
-            image : "url(css/actualsize/ASC.jpg)"
+            image : "url(css/actualsize/specimens.png)"
           },
 
           { 
-            image : "url(css/actualsize/fafw.png)"
+            image : "url(css/actualsize/ASC.png)"
+          },
+
+          { 
+            image : "url(css/actualsize/ASC2.png)"
           },
 
           { 
