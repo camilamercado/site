ac2abd21bbe6b68d503c90cb9c6c342bb6094226
camila mercado <camilamercado11@gmail.com>
2016-08-19 21:05:18 -0400

    add everyhting this is scary

---
+++ software/credentials.js
@@ -0,0 +1,7 @@
+//******SOFTWARE STUDIOS SHOPIFY CREDENTIALS*******
+
+var  SapiKey = 'a9fcd3f4b3fc0ee81eaf097a99d2e81d';
+var  SmyShopifyDomain = 'software-studios';
+var  SappId = '6';
+var Scollection_id = 202599301;
+
